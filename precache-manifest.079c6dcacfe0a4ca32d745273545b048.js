fb472b33643cd38f4f15b6e3cf887624c4b724fc
Roman Agarkov <agarkovromachka@mail.ru>
2020-07-28 22:29:53 +0300

    Updates

---
+++ precache-manifest.079c6dcacfe0a4ca32d745273545b048.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c11afbb99ff053a0c79230cb085632",
+    "url": "/social-network-app/index.html"
+  },
+  {
+    "revision": "ef9051ebf081a21b8c79",
+    "url": "/social-network-app/static/css/main.c61065b4.chunk.css"
+  },
+  {
+    "revision": "339439d8629799cff5e3",
+    "url": "/social-network-app/static/js/2.b1b0e07e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network-app/static/js/2.b1b0e07e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef9051ebf081a21b8c79",
+    "url": "/social-network-app/static/js/main.90c9cca3.chunk.js"
+  },
+  {
+    "revision": "c55f546c296a9b34d006",
+    "url": "/social-network-app/static/js/runtime-main.71d9cc7c.js"
+  },
+  {
+    "revision": "17b4cea8105f1a2c77fc89506889c73e",
+    "url": "/social-network-app/static/media/userIcon.17b4cea8.svg"
+  }
+]);
